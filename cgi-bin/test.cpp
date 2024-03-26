5c792b6af4d24a0e34e64b0088c4aff8cea323a8
volverina <99404411+volverina@users.noreply.github.com>
2024-03-26 14:04:08 +0200

    Add files via upload

--- cgi-bin/test.cpp
+++ cgi-bin/test.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    cout<<"Content-type: text/html"<<endl<<endl;
+
+    cout<<"<!doctype html>\n"
+            "<html>\n"
+            "<head>\n"
+            "<title>Відповідь сервера</title>"
+            "</head>\n"
+            "<body>\n"
+                "Вітаннячко!\n"
+            "</body>";
+    return 0;
+}
